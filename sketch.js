9a40be4a4c6851c6500de14fe9f3469f5d3463c4
Marko Řeháček <marko.rehacek@gmail.com>
2020-10-15 13:43:24 +0100

    multiple circles drawing in polar using perlin noise

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
-//p5.disableFriendlyErrors = true;
+p5.disableFriendlyErrors = true;
 
 const circles = []
+let time = 0;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
-  p5bezier.initBezier(canvas);
+  time = millis();
 
   for (let i = 0; i < 1; i++) {
-    circles.push(new BezierCircle(windowWidth / 2, windowHeight / 2));
+    circles.push(new Circle(width / 2, height / 2));
   }
+
+  sliderNoiseMax = createSlider(0, 10, 5, 0.1);
+  sliderPhase = createSlider(0, 0.01, 0.005, 0.001);
+  sliderNoiseZoff = createSlider(0, 0.01, 0.005, 0.001);
+  sliderMaxCircles = createSlider(0, 500, 20, 1);
+  sliderCircleGrowth = createSlider(-10, 10, 1, 0.1);
 }
+
 
 function draw() {
   background(40);
@@ -18,14 +26,22 @@ function draw() {
   fill(0, 102, 153);
   text(circles.length, 10, 30);
   text("fps: " + frameRate(), 10, 70);
-
+  
+  stroke(255);
+  noFill();
   for (let c of circles) {
-    c.update();
+    c.update(sliderCircleGrowth.value());
     c.draw();
   }
 
-  for (let i = 0; i < 10; i++) {
-    //circles.push(new Circle(random(windowWidth), random(windowHeight), 10));
+  if (millis() > time + 1000)
+  {
+    if (sliderMaxCircles.value() > circles.length) {
+      circles.push(new Circle(windowWidth / 2, windowHeight / 2));
+    }
+    time = millis();
+
+    // todo destroy circles when out of viewer space
   }
 }
 
@@ -35,115 +51,50 @@ function windowResized() {
 
 //
 
-class BezierCircle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.points = [[0, 0]]
+class Circle {
+  constructor(centerX, centerY) {
+    this.centerX = centerX;
+    this.centerY = centerY;
+    
+    this.rMin = 0;
+    this.rMax = 40;
+
+    this.phase = 0.01;
+    this.noiseZoff = 0.1;
+    this.noiseMax = 5;
   }
 
   // todo millis
-  update() {
-    for (let r = 0; r < 360; r += 10) {
-      let x = 100 * sin(r) + 10;
-      let y = 100 * cos(r) + 10;
-      this.points += [x,y];
-    }
+  update(growthRatio) {
+    // for (let r = 0; r < 360; r += 10) {
+    //   let x = 100 * sin(r) + 10;
+    //   let y = 100 * cos(r) + 10;
+    //   this.points += [x,y];
+    // }
 
-    this.bezier = p5bezier.newBezierObj(this.points, "CLOSE", 7);
+    this.noiseZoff += sliderNoiseZoff.value();
+    this.phase += sliderPhase.value();
+    this.noiseMax = sliderNoiseMax.value();
+
+    this.rMin += growthRatio;
+    this.rMax += growthRatio;
   }
 
   draw() {
-    this.bezier.draw();
-    p5bezier.newBezier(pa.get(), 'OPEN', 9)
-  }
-}
+    push();
+    translate(this.centerX, this.centerY);
 
-
-
-//////////////////////////////////////////
-
-let pa // a PointArray object
-
-function setup() {
-  let c = createCanvas(windowWidth, windowHeight)
-  p5bezier.initBezier(c)
-  noFill()
-
-  pa = new PointArray()
-  pa.add(100, 100)
-  pa.add(400, 600)
-  pa.add(500, 400)
-  pa.add(600, 600)
-  pa.add(900, 100)
-}
-
-function draw() {
-  background(255)
-  stroke(color('#FD5E53'))
-  strokeWeight(3)
-  // Draw an open Bezier curve with fidelity of 9 (highest)
-  p5bezier.newBezier(pa.get(), 'OPEN', 9)
-  pa.display()
-}
-
-function mouseDragged() {
-  pa.update(mouseX, mouseY)
-}
-
-class PointArray {
-  constructor() {
-    this.pointArray = []
-  }
-
-  add(pX, pY) {
-    this.pointArray.push(new Point(pX, pY))
-  }
-
-  display() {
-    for (let point of this.pointArray) point.display()
-  }
-
-  update(mX, mY) {
-    for (let point of this.pointArray) {
-      if (dist(mX, mY, point.position[0], point.position[1]) < 60)
-        point.update(mX, mY)
+    beginShape();
+    for (let a = 0; a < TWO_PI; a+=0.1) {
+      let xoff = map(cos(a+this.phase), -1, 1, 0, this.noiseMax);
+      let yoff = map(sin(a+this.phase), -1, 1, 0, this.noiseMax);
+      let r = map(noise(xoff, yoff, this.noiseZoff), 0, 1, this.rMin, this.rMax);
+      let x = r * cos(a);
+      let y = r * sin(a);
+      vertex(x, y);
     }
-  }
-
-  get() {
-    // Return an array of array
-    // for p5bezier.newBezier function to use
-    let positionArray = []
-    // point is a Point object
-    for (let point of this.pointArray) positionArray.push(point.position)
-    return positionArray
-  }
-}
-
-class Point {
-  constructor(x, y) {
-    this.position = [x, y]
-    this.r = 10
-  }
-
-  display() {
-    push()
-    fill(255)
-    stroke(0)
-    strokeWeight(2)
-    ellipse(this.position[0], this.position[1], 2 * this.r, 2 * this.r)
-    fill(0)
-    noStroke()
-    text(
-      '(' + this.position[0] + ', ' + this.position[1] + ')',
-      this.position[0] + 10,
-      this.position[1] - 10
-    )
-    pop()
-  }
-
-  update(mX, mY) {
-    this.position = [mX, mY]
+    endShape(CLOSE);
+    
+    pop();
   }
 }
